a86920bc40ef9280666f004623be8abfe7f5473a
Anirup Patnaik <anirup049@gmail.com>
2019-11-15 14:46:34 +0530

    Initial Commit

---
+++ FirstSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Anirup Patnaik on 15/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
